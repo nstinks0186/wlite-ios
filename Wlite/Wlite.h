10bd2a3b086d1daadbf4a4791dce033b70546642
Pinuno Fuentes <nstinks0186@gmail.com>
2015-07-24 10:28:20 -0400

    Initial Commit

---
+++ Wlite/Wlite.h
@@ -0,0 +1,19 @@
+//
+//  Wlite.h
+//  Wlite
+//
+//  Created by Pinuno Fuentes on 7/24/15.
+//  Copyright (c) 2015 Pinuno Fuentes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Wlite.
+FOUNDATION_EXPORT double WliteVersionNumber;
+
+//! Project version string for Wlite.
+FOUNDATION_EXPORT const unsigned char WliteVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Wlite/PublicHeader.h>
+
+
